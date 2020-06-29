5b319b3befd0806e2ac115661845b7513c842c58
Thibaut Salence <salencethibaut95@gmail.com>
2020-06-29 10:36:12 +0200

    Initial Commit

---
+++ pathfinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pathfinder
+//
+//  Created by Thibaut Salence on 29/06/2020.
+//  Copyright © 2020 Thibaut Salence. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
